71a63a9611cb01aff80de92cfb7ebc219cfd70e3
Hector Hernandez Morales <hectorhm1596@gmail.com>
2019-04-03 04:05:56 -0700

    Has many through already working. Implemented in Area missing in Proyecto. Area has sample code and how it works.

--- CommandsAndChanges.md
+++ CommandsAndChanges.md
@@ -1,3 +1,13 @@
+# 2 April 2019
+	* Fixed hopefully last issues with eloquent tables.
+		* Many to Many already working, with sample so as to properly create more.
+			* One to Many
+			* One to One
+		* Routes already for the provided scenarios.
+	* Also finished with the modification to the table name of the pivot table of "areas_proyectos".
+		* It had the wrong name before, messing up the relations.
+		* This table will be used for the HasManyThrough
+
 # 1 April 2019 
 	* Fixed issue with tables.
 		* They had wrong columns for some attributes I didn't notice before.
@@ -5,7 +15,7 @@
 		* Modified the web.php in route so that it's usable again.
 	* Continued with web routes and controllers.
 		* Added routes with eloquent for the following:
-			* 
+
 # 27 March 2019 7:33pm
 	* Added to the migrations the foreign keys needed.
 		1.  Used 
--- app/Area.php
+++ app/Area.php
@@ -27,6 +27,11 @@ class Area extends Model
 
     public function proyectos()
     {
-        return $this->belongsToMany('App\Proyecto');
+        return $this->hasManyThrough('App\Proyecto',    // A que modelo voy
+                                    'App\AreaProyecto', // Que modelo me va a ayudar
+                                    'area_id',          // Cual es con la que me voy a comunicar con la llave del que parto (aka en la tabla de area_proyecto)
+                                    'id',               // Como se llama con la llave que comparo en la tabla final (aka proyectos)
+                                    'id',               // Llave con la que parto de la tabla inicial (aka areas)
+                                    'proyecto_id');     // Cual es la llave con la que parto de la tabla media a la final (aka area_proyecto)
     }
 }
